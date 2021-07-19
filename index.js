91b0871fa4ef6c5cf4d92027080919243ad897b0
prashant-kumar1330 <kumarprashant1330@gmail.com>
2021-07-20 02:21:56 +0530

    first commit

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const emailForm = document.querySelector("#emailForm");
 const toast = document.querySelector(".toast");
 
 const baseURL = "https://fileshare-pk.herokuapp.com";
-const uploadURL = `${baseURL}/api/files`;
+const uploadURL = `${baseURL}/api/file`;
 const emailURL = `${baseURL}/api/files/send`;
 
 const maxAllowedSize = 100 * 1024 * 1024; //100mb
